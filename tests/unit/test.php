ce37bb4331f63d917a2a3a063b1f4b805b6fd01c
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-27 23:17:35 +0100

    Update test.php
    fix: remove the hash and the query on it

--- tests/unit/test.php
+++ tests/unit/test.php
@@ -3,30 +3,16 @@
     $needle = '<p class="blubb">';
     $replacement = '<p class="bla">';
     $whichOccurs = '1,3';
-    $hash = 'HJGHKHJGXDFHJ258632154'; // md5-hash der Datei
-
+    
     $res = array();
 
     $replacement = new FileProcessor();
     if ($replacement->setFile($file) === true) {
-        if ($hash == $replacement->getFileHash()) {
-            $replacement->setNeedle($needle);
-            $replacement->setReplacement($replacement);
-            $replacement->setWhichOccurs($whichOccurs);
+        $replacement->setNeedle($needle);
+        $replacement->setReplacement($replacement);
+        $replacement->setWhichOccurs($whichOccurs);
 
-            $res[] = $replacement->patch();
-
-        } else {
-
-            $res[] = array('file' => $file,
-                           'needle' => $needle,
-                           'replacement' => $replacement,
-                           'whichOccurs' => $whichOccurs,
-                           'message' => 'Datei nicht original! Die Installation muß manuell abgeschlossen werden.',
-                           'error' => true
-                           ); // todo auslagern in Klasse zur Verwaltung und Formatierung der Rückmeldung
-
-        }
+        $res[] = $replacement->patch();
     }
 
     $chk_res = array_keys(array $res, true, true); // return array
